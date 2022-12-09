fc772bfc6ed04546d2fd584fe76e32168c83984d
Toby Mackenzie <public@tobymackenzie.com>
2022-12-09 13:05:48 -0500

    build: test `file_put_contents` for build

--- build.php
+++ build.php
@@ -6,19 +6,15 @@ if(getenv('GITHUB_ACTIONS')){
 }else{
 	$out .= "not Github action\n\n";
 }
-$out .= "ENV\n-----\n\n";
-$out .= "```\n";
 $out .= json_encode($_ENV, JSON_PRETTY_PRINT) . "\n";
 $out .= "```\n";
 $out .= "Github ENV variables\n-----\n\n";
 $out .= "GITHUB_RUN_ID: " . getenv('GITHUB_RUN_ID') . "\n";
 $out .= "GITHUB_RUN_NUMBER: " . getenv('GITHUB_RUN_NUMBER') . "\n";
 $out .= "GITHUB_SHA: " . getenv('GITHUB_SHA') . "\n";
-$out .= "GITHUB_WORKSPACE: " . getenv('GITHUB_WORKSPACE') . "\n";
 $out .= "pwd: " . shell_exec('pwd') . "\n";
 
 $out .= "\nran at: " . (new DateTime())->format('Ymd H:i:s') . "\n";
 
 
-// file_put_contents((getenv('GITHUB_WORKSPACE') ?: __DIR__) . '/dist/build.txt', $out);
-echo $out;
+file_put_contents(__DIR__ . '/dist/build.txt', $out);
